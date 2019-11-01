ac06e5f292540933eed2ea1fc7f44fbd8645daae
Ilija Djukic <ilija.djukic@rt-rk.com>
2019-11-01 15:08:38 +0100

    demo touch events

--- demo/src/demo.cpp
+++ demo/src/demo.cpp
@@ -8,12 +8,7 @@ static struct
     vec3_t pos;
     vec3_t rot;
     bool auto_rotate;
-} camera =
-{
-    { 0, 0, -6 },
-    { -45, 0, 40},
-    false
-};
+} camera;
 
 static BCFont *font = NULL;
 
@@ -32,6 +27,8 @@ static std::vector<Object> objs;
 
 extern "C" void BC_onConfig(BCConfig *config)
 {
+    bcLogDebug("BC_onConfig");
+
     float aspect = (float) config->width / (float) config->height;
 #ifndef __ANDROID__
     config->height = 480;
@@ -44,14 +41,27 @@ extern "C" void BC_onConfig(BCConfig *config)
 
 extern "C" void BC_onCreate()
 {
+    bcLogDebug("BC_onCreate");
+
+    bcLogDebug("Build time: %s %s", __DATE__, __TIME__);
+
+    camera =
+    {
+        { 0, 0, -6 },
+        { -45, 0, 40},
+        false
+    };
 }
 
 extern "C" void BC_onDestroy()
 {
+    bcLogDebug("BC_onDestroy");
 }
 
 extern "C" void BC_onStart()
 {
+    bcLogDebug("BC_onStart");
+
     font = bcCreateFontTTF(ASSETS_DIR"vera.ttf", 30);
 
     objs.push_back(
@@ -76,23 +86,18 @@ extern "C" void BC_onStart()
 
 extern "C" void BC_onStop()
 {
+    bcLogDebug("BC_onStop");
+
     bcDestroyFont(font);
     for (Object & obj : objs)
     {
         bcDestroyMesh(obj.mesh);
-        obj.mesh = NULL;
     }
+    objs.clear();
 }
 
 extern "C" void BC_onUpdate(float dt)
 {
-    // camera control
-    if (bcIsMouseDown(0))
-    {
-        camera.rot.z += bcGetMouseDeltaX();
-        camera.rot.x += bcGetMouseDeltaY();
-    }
-    camera.pos.z += bcGetMouseWheel() * 0.1f;
     if (camera.auto_rotate)
     {
         camera.rot.z += dt * 50;
@@ -138,8 +143,12 @@ extern "C" void BC_onUpdate(float dt)
 extern "C" void BC_onEvent(BCEvent event)
 {
     static bool wire = false;
-    if (event.type == BC_EVENT_KEY_RELEASE)
+    static bool do_rotate = false;
+    static vec2i_t last = { 0, 0 };
+
+    switch (event.type)
     {
+    case BC_EVENT_KEY_RELEASE:
         switch (event.id)
         {
         case BC_KEY_ESCAPE:
@@ -153,5 +162,35 @@ extern "C" void BC_onEvent(BCEvent event)
             camera.auto_rotate = !camera.auto_rotate;
             break;
         }
+        break;
+    case BC_EVENT_MOUSE_PRESS:
+    case BC_EVENT_TOUCH_DOWN:
+        if (event.id == 0)
+        {
+            do_rotate = true;
+            last = { event.x, event.y };
+        }
+        break;
+    case BC_EVENT_MOUSE_RELEASE:
+    case BC_EVENT_TOUCH_UP:
+        if (event.id == 0)
+        {
+            do_rotate = false;
+        }
+        break;
+    case BC_EVENT_MOUSE_MOVE:
+    case BC_EVENT_TOUCH_MOVE:
+        if (do_rotate)
+        {
+            int dx = event.x - last.x;
+            int dy = event.y - last.y;
+            camera.rot.z += dx;
+            camera.rot.x += dy;
+            last = { event.x, event.y };
+        }
+        break;
+    case BC_EVENT_MOUSE_WHEEL:
+        camera.pos.z += event.y * 0.1f;
+        break;
     }
 }
